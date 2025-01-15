42a65de8838b8fc0e957ee997c4948e1288576e8
Mihaly Farkas <194117456+mihaly-farkas@users.noreply.github.com>
2025-01-15 04:26:11 +0100

    ...

--- .scripts/verify.sh
+++ .scripts/verify.sh
@@ -12,24 +12,33 @@ function copy() {
   SOURCE=$1
   DEST=$2
 
+  set -x
   copier copy "$SOURCE" "$DEST" \
     --defaults \
     --force \
     --overwrite \
     --vcs-ref=HEAD
+  { set +x; } 2>/dev/null
+}
+
+function compare() {
+  GENERATED=$1
+  FIXTURES=$2
+
+  set -x
+  diff --recursive "$GENERATED" "$FIXTURES" --exclude=.copier-answers.yml --exclude=.fixtures || $DIFFERENT
+  { set +x; } 2>/dev/null
 }
 
 function test_template() {
   TEMPLATE=$1
 
   copy . ".generated/template-$TEMPLATE"
-
-  diff --recursive ".generated/template-$TEMPLATE" ".fixtures/template-$TEMPLATE" --exclude=.copier-answers.yml --exclude=.fixtures || $DIFFERENT
+  compare ".generated/template-$TEMPLATE" ".fixtures/template-$TEMPLATE"
 
   copy ".generated/template-$TEMPLATE" ".generated/template-$TEMPLATE.generated/project-default"
-
-  diff --recursive ".generated/template-$TEMPLATE.generated/project-default" ".fixtures/template-$TEMPLATE/.fixtures/project-default" --exclude=.copier-answers.yml || $DIFFERENT
-  diff --recursive ".generated/template-$TEMPLATE.generated/project-default" ".fixtures/template-$TEMPLATE.generated/project-default" --exclude=.copier-answers.yml || $DIFFERENT
+  compare ".generated/template-$TEMPLATE.generated/project-default" ".fixtures/template-$TEMPLATE/.fixtures/project-default"
+  compare ".generated/template-$TEMPLATE.generated/project-default" ".fixtures/template-$TEMPLATE.generated/project-default"
 }
 
 test_template "default"
